13352188eb133d1b0cd95dc1009c8bd4e809aab1
ArdaBehramuni <arda.behram@std.yeditepe.edu.tr>
2025-11-01 12:10:00 +0300

    We find the period of pendulum for 1m to 10m using for function

---
+++ assignment1.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def find_period(L_0, L_1):
+    g = 9.81 #m/s^2
+    T_0 = 2 * np.pi * np.sqrt(L_0/g)
+    T_1 = 2 * np.pi * np.sqrt(L_1/g)
+    
+    for L in range(L_0, L_1+1, 1):
+        T = 2 * np.pi * np.sqrt(L / g)
+        print("When L = %.1f m, T = %.1f s" % (L, T))
+    
+    return (float(round(T_0, 1)), float(round(T_1, 1)))
